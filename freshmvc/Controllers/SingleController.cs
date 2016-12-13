5b3984c282c80b3c8d67506145357ee0e0096955
Christopher <Christopher@DESKTOP-PG925NI>
2016-12-13 08:25:10 -0500

    Add project files.

---
+++ freshmvc/Controllers/SingleController.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace freshmvc.Controllers
+{
+    public class SingleController : Controller
+    {
+        // GET: Single
+        public ActionResult Index()
+        {
+            Database Database = Sitecore.Configuration.Factory.GetDatabase("master");
+
+            Item item = Sitecore.Context.Database.Items["{0068464F-BD66-495A-9534-ABA45BF5D563}"];
+
+
+            return View(item);
+        }
+    }
+}
---
+++ freshmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="freshmvc.MvcApplication" Language="C#" %>
